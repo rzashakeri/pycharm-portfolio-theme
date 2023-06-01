5bdd259efdcea7c48ca191b90f61ae6b3f9d5a33
Reza shakeri <rzashakeri@gmail.com>
2023-06-01 06:07:27 +0330

    ✨ feat: changed title of page when user go to another page

--- Front End/assets/js/index.js
+++ Front End/assets/js/index.js
@@ -78,3 +78,11 @@ function changIcon() {
     }
 }
 
+let docTitle = document.title;
+window.addEventListener("blur", () => {
+    document.title = "Come Back :)";
+});
+
+window.addEventListener("focus", () => {
+    document.title = docTitle;
+});
