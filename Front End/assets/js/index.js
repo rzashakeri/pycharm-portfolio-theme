f0bcd01343054c8608c527c517b6a6d0f20df9b5
Reza shakeri <rzashakeri@gmail.com>
2023-06-04 18:58:23 +0330

    ♻️ refactor: removed unused js from index.js

--- Front End/assets/js/index.js
+++ Front End/assets/js/index.js
@@ -1,40 +1,3 @@
-setInterval(() => $("#dateTime").text(new Date().toLocaleString()), 1000);
-
-
-const resizer = document.querySelector("#resizer");
-const sidebar = document.querySelector("#sidebar");
-
-resizer.addEventListener("mousedown", (event) => {
-    document.addEventListener("mousemove", resize, false);
-    document.addEventListener(
-        "mouseup",
-        () => {
-            document.removeEventListener("mousemove", resize, false);
-        },
-        false
-    );
-});
-
-function resize(e) {
-    const size = `${e.x}px`;
-    sidebar.style.flexBasis = size;
-}
-
-function showContent() {
-    let element = document.querySelector(".sidebar-content");
-    let sideBar = document.querySelector("#sidebar")
-    if (element.style.display === "none") {
-        element.setAttribute(
-            "style",
-            "display: flex; flex-direction: column; width: 210px;"
-        );
-    } else {
-        element.setAttribute("style", "display: none;");
-        sideBar.removeAttribute("style");
-
-    }
-}
-
 
 $(".main-menu-head-title").codex({
     effect: "charbychar",
@@ -60,29 +23,3 @@ function ReadMoreAboutText() {
         moreText.style.display = "inline";
     }
 }
-
-function changFirstHeaderIcon() {
-    let element = document.querySelector(".sidebar-first-header-icon");
-    let sidebarItems = document.querySelector(".sidebar-items");
-    if (element.className == "sidebar-first-header-icon") {
-        element.setAttribute(
-            "src",
-            "./assets/images/icons/findAndShowNextMatches.svg"
-        );
-        element.className += " open";
-        sidebarItems.removeAttribute("style");
-    } else {
-        element.setAttribute("src", "./assets/images/icons/play_forward.svg");
-        element.className = "sidebar-first-header-icon";
-        sidebarItems.setAttribute("style", "display: none;");
-    }
-}
-
-let docTitle = document.title;
-window.addEventListener("blur", () => {
-    document.title = "Come Back :)";
-});
-
-window.addEventListener("focus", () => {
-    document.title = docTitle;
-});
