6523cfa4427303402fa0ff4ec7fc82ed91bc2778
Reza shakeri <rzashakeri@gmail.com>
2023-06-07 08:53:07 +0330

    ✨ feat: added contact us model

--- Back End/contact_us/models.py
+++ Back End/contact_us/models.py
@@ -1,3 +1,9 @@
+from django.core import validators
 from django.db import models
 
-# Create your models here.
+
+class ContactUs(models.Model):
+	first_name = models.CharField(max_length=50)
+	last_name = models.CharField(max_length=50)
+	email = models.EmailField(max_length=250, validators=[validators.validate_email])
+	message = models.TextField()
