722f4c52b01b28dfd1116ecc8beba55ecafc2f10
Reza shakeri <rzashakeri@gmail.com>
2023-06-07 08:18:50 +0330

    ✨ feat: added about model

--- Back End/core/models.py
+++ Back End/core/models.py
@@ -4,3 +4,8 @@ from django.db import models
 class Site(models.Model):
 	title = models.CharField(max_length=200)
 	job_title = models.CharField(max_length=200)
+
+
+class About(models.Model):
+	title = models.CharField(max_length=100)
+	body = models.TextField()
