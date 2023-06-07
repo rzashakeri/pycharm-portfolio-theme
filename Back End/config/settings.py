15e79f1db12871f842e57adffca7fd3f0f6564d6
Reza shakeri <rzashakeri@gmail.com>
2023-06-07 08:17:36 +0330

    ✨ feat: added apps to settings.py

--- Back End/config/settings.py
+++ Back End/config/settings.py
@@ -35,7 +35,10 @@ INSTALLED_APPS = [
 	'django.contrib.sessions',
 	'django.contrib.messages',
 	'django.contrib.staticfiles',
-	'core'
+	'core',
+	'project',
+	'contact_us',
+	'pages'
 ]
 
 MIDDLEWARE = [
